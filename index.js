885f708a94b13b6498af85ce406fc6b4b3d3a5b9
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-18 15:17:49 -0500

    Configuracion inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+function main() {
+  const container = document.querySelector("#app");
+  const h1 = document.createElement("h1");
+  h1.textContent = "HTML - TESTING";
+  container.appendChild(h1);
+}
+
+main();
