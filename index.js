57aa99a6bb2b6dc91ed90b1d4c21612bfcaf4975
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-18 15:25:44 -0500

    Pase el h1 que se generaba en el js a el html

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function main() {
-  const container = document.querySelector("#app");
-  const h1 = document.createElement("h1");
-  h1.textContent = "HTML - TESTING";
-  container.appendChild(h1);
+  // const container = document.querySelector("#app");
 }
 
 main();
